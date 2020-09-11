e90df494c0f884441e408fc148bd68d6441ee15a
aliasgharmhowwala <aliasghar17161@gmail.com>
2020-09-11 16:21:11 +0000

    Added Formatted the files

--- ultradns/provider_test.go
+++ ultradns/provider_test.go
@@ -6,10 +6,37 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 )
 
 var testAccProviders map[string]terraform.ResourceProvider
 var testAccProvider *schema.Provider
+
+func setSchemaProvider() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"username": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ULTRADNS_USERNAME", nil),
+				Description: "UltraDNS Username.",
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ULTRADNS_PASSWORD", nil),
+				Description: "UltraDNS User Password",
+			},
+			"baseurl": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("ULTRADNS_BASEURL", nil),
+				Description: "UltraDNS Base URL",
+			},
+		},
+	}
+}
 
 func init() {
 	testAccProvider = Provider().(*schema.Provider)
@@ -28,6 +55,29 @@ func TestProvider_impl(t *testing.T) {
 	var _ terraform.ResourceProvider = Provider()
 }
 
+func TestProviderConfigure(t *testing.T) {
+
+	resourceRecordObj := setSchemaProvider()
+	resourceData := resourceRecordObj.TestResourceData()
+
+	resourceData.Set("username", "abcd")
+	resourceData.Set("password", "abcd")
+	resourceData.Set("baseurl", "abcd")
+
+	config := Config{
+		Username: "abcd",
+		Password: "abcd",
+		BaseURL:  "abcd",
+	}
+
+	expected, _ := config.Client()
+
+	actualData, _ := providerConfigure(resourceData)
+	log.Infof("%#v", actualData)
+	assert.Equal(t, expected, actualData, true)
+
+}
+
 func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("ULTRADNS_USERNAME"); v == "" {
 		t.Fatal("ULTRADNS_USERNAME must be set for acceptance tests")
@@ -44,5 +94,4 @@ func testAccPreCheck(t *testing.T) {
 	if v := os.Getenv("ULTRADNS_BASEURL"); v == "" {
 		t.Fatal("ULTRADNS_BASEURL must be set for acceptance tests")
 	}
-
 }
